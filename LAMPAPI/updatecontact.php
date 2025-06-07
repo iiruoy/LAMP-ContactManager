7f74c4587171a2c12e8bbaf96ef2031ca1059d9a
bobobubby <132294343+bobobubby@users.noreply.github.com>
2025-06-06 23:21:50 -0400

    Add files via upload

---
+++ LAMPAPI/updatecontact.php
@@ -0,0 +1,46 @@
+<?php
+	$inData = getRequestInfo();
+	
+	#$userID = $inData["UserID"];
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$Email = $inData["Email"];
+	$Phone = $inData["Phone"];
+	$Company = $inData["Company"];
+	$contactID = $inData["ID"];
+
+	$conn = mysqli_connect("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	
+	
+	if($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error);
+	}
+	else
+	{
+		
+		$sql1 = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Phone = ?, Email = ?, Company = ? WHERE ID = ?");
+		$sql1->bind_param("ssssss",$FirstName,$LastName,$Phone,$Email,$Company,$contactID);
+		$sql1->execute();
+
+		returnWithError("");
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'),true);
+	}
+	
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"'. $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
